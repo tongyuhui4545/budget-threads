102f72c52df3f1a180f0a2ae3b2706c9321818e6
Sam <tongyuhui@gmail.com>
2025-01-26 10:04:29 +1100

    feat: prisma fetching data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import sampleData from "@/db/sample-data"
 import ProductList from "@/components/shared/product/product-list"
+import {getLatestProducts} from "@/lib/actions/product.actions";
 
 const Homepage = async () => {
+  const latestProducts = await getLatestProducts()
   return (
     <>
-      <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </>
   )
 }
